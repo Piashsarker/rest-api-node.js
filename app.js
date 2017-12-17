100bb4efaf3262a2a427792bd961a9475d8aa86f
Piash Sarker <sarkerpt@gmail.com>
2017-12-17 12:13:09 +0600

    Access-Control-Allow-Origin Added

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ mongoose.connect('mongodb://piashsarker:piash12345@ds249415.mlab.com:49415/ninja
   useMongoClient : true
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(bodyParser.json());
 //initialize routes
